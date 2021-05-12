db0a08539a8ab3a9d46e3f868fbf7df690d065a6
Administrator <Administrator@WJTHGW3070KDBIK>
2021-05-12 22:00:03 +0700

    Init repository

---
+++ webdriver/api/Topic02_Check_Environment.java
@@ -0,0 +1,34 @@
+package api;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+
+public class Topic02_Check_Environment {
+	WebDriver driver;
+	@BeforeClass
+	public void beforeClass() {
+		//mở app
+		driver = new FirefoxDriver();
+		driver.get("https://github.com/");
+		}
+	
+  @Test
+  public void test_case_01() {
+  }
+  @Test
+  public void test_case_02() {
+  }
+  @Test
+  public void test_case_03() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  //Đóng trình duyệt
+	  driver.quit();
+  }
+
+}
