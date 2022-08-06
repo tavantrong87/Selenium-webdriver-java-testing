858f20b689f88123add63f6fbde1cd08d63343c2
Trong Ta <tavantrong87@gmail.com>
2022-08-06 12:30:36 +0700

    Edit data

---
+++ webdriver/api/Xpath_Css_Locator.java
@@ -0,0 +1,19 @@
+package api;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Xpath_Css_Locator {
+  @Test
+  public void f() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
